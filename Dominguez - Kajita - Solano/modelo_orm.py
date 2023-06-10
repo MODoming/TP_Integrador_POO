81e5bda721c028150b16de94e7d08984587e824d
Maximiliano <91306377+MODoming@users.noreply.github.com>
2023-06-10 01:38:06 -0300

    Iniciando
    Creando repositorio para iniciar los trabajos referidos al armado del Trabajo Practico Integrador.

---
+++ Dominguez - Kajita - Solano/modelo_orm.py
@@ -0,0 +1,17 @@
+# Instituto de Formación Técnica Superior Nro 18 - Profesor Eduardo Iberti 
+# CARRERA de TÉCNICO SUPERIOR en DESARROLLO DE SOFTWARE - Trabajo Práctico Final Integrador
+# Integrantes Dominguez Maximiliano, Kajita Lucas, Solano Santiago
+
+'''5. La clase “Obra”, que es una de las clases que debe formar parte del modelo ORM, debe incluir
+    los siguientes métodos de instancia con el objetivo de definir las diferentes etapas de avance
+    de obra:
+        a. nuevo_proyecto().
+        b. iniciar_contratacion().
+        c. adjudicar_obra().
+        d. iniciar_obra().
+        e. actualizar_porcentaje_avance().
+        f. incrementar_plazo().
+        g. incrementar_mano_obra().
+        h. finalizar_obra().
+        i. rescindir_obra().'''
+
