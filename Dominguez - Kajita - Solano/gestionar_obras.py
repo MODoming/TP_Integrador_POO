0ffe5e4dbd97053228b2c8024ca838acbc745e28
Max <maxi.dmgz@gmail.com>
2023-06-15 20:18:13 -0300

    Avanzando con el TP

--- Dominguez - Kajita - Solano/gestionar_obras.py
+++ Dominguez - Kajita - Solano/gestionar_obras.py
@@ -1,34 +1,28 @@
 # Instituto de Formación Técnica Superior Nro 18 - Profesor Eduardo Iberti 
 # CARRERA de TÉCNICO SUPERIOR en DESARROLLO DE SOFTWARE - Trabajo Práctico Final Integrador
 # Integrantes Dominguez Maximiliano, Kajita Lucas, Solano Santiago
+# Tema: Sistema de gestión de obras públicas con manejo de POO, importación de datasets desde un archivo csv y persistencia de objetos con ORM Peewee en una base de datos SQLite.
 
 '''Crear otro módulo “gestionar_obras.py” que contenga la definición de la clase abstracta
-    “GestionarObra” y los siguientes métodos de clase:
-    a. extraer_datos(), que debe incluir las sentencias necesarias para manipular el dataset a
-        través de un objeto Dataframe del módulo “pandas”.
-    b. conectar_db(), que debe incluir las sentencias necesarias para realizar la conexión a la
-        base de datos “obras_urbanas.db”.
-    c. mapear_orm(), que debe incluir las sentencias necesarias para realizar la creación de la
-        estructura de la base de datos (tablas y relaciones) utilizando el método de instancia
-        “create_tables(list)” del módulo peewee.
-    d. limpiar_datos(), que debe incluir las sentencias necesarias para realizar la “limpieza” de
-        los datos nulos y no accesibles del Dataframe.
-    e. cargar_datos(), que debe incluir las sentencias necesarias para persistir los datos de las
-        obras (ya transformados y “limpios”) que contiene el objeto Dataframe en la base de datos
-        relacional SQLite. Para ello se debe utilizar el método de clase Model create() en cada una
-        de las clase del modelo ORM definido.
-    f. nueva_obra(), que debe incluir las sentencias necesarias para crear nuevas instancias de
-        Obra. Se deben considerar los siguientes requisitos:
-            • Todos los valores requeridos para la creación de estas nuevas instancias deben ser
-            ingresados por teclado.
-            • Para los valores correspondientes a registros de tablas relacionadas (foreign key), el
-            valor ingresado debe buscarse en la tabla correspondiente mediante sentencia de
-            búsqueda ORM, para obtener la instancia relacionada, si el valor ingresado no existe
-            en la tabla, se le debe informar al usuario y solicitarle un nuevo ingreso por teclado.
-            • Para persistir en la BD los datos de la nueva instancia de Obra debe usarse el método
-            save() de Model del módulo peewee.
-            • Este método debe retornar la nueva instancia de obra.
-    g. obtener_indicadores(), que debe incluir las sentencias necesarias para obtener
-        información de las obras existentes en la base de datos SQLite a través de sentencias
-        ORM.'''
+    “GestionarObra” y los siguientes métodos de clase:'''
+
+class GestionarObra():    
+    def extraer_datos(): # que debe incluir las sentencias necesarias para manipular el dataset a través de un objeto Dataframe del módulo “pandas”.
+        pass
+    def conectar_db(): # que debe incluir las sentencias necesarias para realizar la conexión a la base de datos “obras_urbanas.db”.
+        pass
+    def mapear_orm(): # que debe incluir las sentencias necesarias para realizar la creación de la estructura de la base de datos (tablas y relaciones) utilizando el método de instancia “create_tables(list)” del módulo peewee.
+        pass
+    def limpiar_datos(): # que debe incluir las sentencias necesarias para realizar la “limpieza” de los datos nulos y no accesibles del Dataframe.
+        pass
+    def cargar_datos(): # que debe incluir las sentencias necesarias para persistir los datos de las obras (ya transformados y “limpios”) que contiene el objeto Dataframe en la base de datos relacional SQLite. Para ello se debe utilizar el método de clase Model create() en cada una de las clase del modelo ORM definido.
+        pass
+    def nueva_obra(): # que debe incluir las sentencias necesarias para crear nuevas instancias de Obra. Se deben considerar los siguientes requisitos:
+            #• Todos los valores requeridos para la creación de estas nuevas instancias deben ser ingresados por teclado.
+            #• Para los valores correspondientes a registros de tablas relacionadas (foreign key), el valor ingresado debe buscarse en la tabla correspondiente mediante sentencia de búsqueda ORM, para obtener la instancia relacionada, si el valor ingresado no existe en la tabla, se le debe informar al usuario y solicitarle un nuevo ingreso por teclado.
+            #• Para persistir en la BD los datos de la nueva instancia de Obra debe usarse el método save() de Model del módulo peewee.
+            #• Este método debe retornar la nueva instancia de obra.
+        pass
+    def obtener_indicadores(): # que debe incluir las sentencias necesarias para obtener información de las obras existentes en la base de datos SQLite a través de sentencias ORM.'''
+        pass
 
--- Dominguez - Kajita - Solano/modelo_orm.py
+++ Dominguez - Kajita - Solano/modelo_orm.py
@@ -1,17 +1,45 @@
 # Instituto de Formación Técnica Superior Nro 18 - Profesor Eduardo Iberti 
 # CARRERA de TÉCNICO SUPERIOR en DESARROLLO DE SOFTWARE - Trabajo Práctico Final Integrador
 # Integrantes Dominguez Maximiliano, Kajita Lucas, Solano Santiago
+# Tema: Sistema de gestión de obras públicas con manejo de POO, importación de datasets desde un archivo csv y persistencia de objetos con ORM Peewee en una base de datos SQLite.
 
-'''5. La clase “Obra”, que es una de las clases que debe formar parte del modelo ORM, debe incluir
-    los siguientes métodos de instancia con el objetivo de definir las diferentes etapas de avance
-    de obra:
-        a. nuevo_proyecto().
-        b. iniciar_contratacion().
-        c. adjudicar_obra().
-        d. iniciar_obra().
-        e. actualizar_porcentaje_avance().
-        f. incrementar_plazo().
-        g. incrementar_mano_obra().
-        h. finalizar_obra().
-        i. rescindir_obra().'''
+'''3. Crear el módulo “modelo_orm.py” que contenga la definición de las clases y sus atributos correspondientes que considere necesarios, siguiendo el modelo ORM de Peewee para poder persistir los datos importados del dataset en una base de datos relacional de tipo SQLite llamada “obras_urbanas.db”, ubicada en la misma carpeta solución del proyecto. Aquí se debe incluir además la clase BaseModel heredando de peewee.Model.'''
+
+from peewee import *
+
+data_base = SqliteDatabase('obras_urbanas.db')
+class BaseModel(Model):
+    class Meta:
+        database = data_base
+
+'''5. La clase “Obra”, que es una de las clases que debe formar parte del modelo ORM, debe incluir los siguientes métodos de instancia con el objetivo de definir las diferentes etapas de avance de obra:'''
+
+class Obra(BaseModel):
+    id = AutoField()
+    nombre = CharField()
+    tipo = CharField()
+    ubicacion = CharField()
+    fecha_inicio = DateField()
+    fecha_fin = DateField(null=True)
+    costo = IntegerField()
+
+
+    def nuevo_proyecto():
+        pass
+    def iniciar_contratacion():
+        pass
+    def adjudicar_obra():
+        pass
+    def iniciar_obra():
+        pass
+    def actualizar_porcentaje_avance():
+        pass
+    def incrementar_plazo():
+        pass
+    def incrementar_mano_obra():
+        pass
+    def finalizar_obra():
+        pass
+    def rescindir_obra():
+        pass
 
